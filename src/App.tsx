deef3aa01c507e31ef49a740b370de16e2f86321
Julia Arrington <jarring.dev@gmail.com>
2026-02-12 10:08:48 -0700

    added sort feature and some styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
 import './App.css'
 import React, { useState, useEffect } from 'react';
 import Card from './components/card'
+import sortAZ from './assets/alphabetical-sorting-icon-lg.png'
+import sortZA from './assets/alphabetical-sorting-icon-size_24.png'
+
+interface Product {
+  id: number;
+  image: string;
+  title: string;
+  price: number;
+  description: string;
+}
 
 function App() {
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState<string | null>(null);
-  const [data, setData] = useState(null);
+  const [data, setData] = useState<Product[] | null>(null);
+  const [atoZ, setAtoZ] = useState(true);
 
   const fetchData = () => {
     setLoading(true);
@@ -35,19 +46,73 @@ function App() {
     fetchData();
   }, []);
 
-console.log('data', data);
+//console.log('data', data);
+
+function sortAtoZ() {
+  const sortedData = data ? data.sort(
+    (a, b) => {
+    const titleA = a.title.toUpperCase(); // ignore upper and lowercase
+    const titleB = b.title.toUpperCase(); // ignore upper and lowercase
+    if (titleA < titleB) {
+      return -1;
+    }
+    if (titleA > titleB) {
+      return 1;
+    }
+
+    // names must be equal
+    return 0;
+  }
+  ) : [];}
+
+function sortZtoA() {
+  const sortedData = data ? data.sort(
+    (a, b) => {
+    const titleA = a.title.toUpperCase(); // ignore upper and lowercase
+    const titleB = b.title.toUpperCase(); // ignore upper and lowercase
+    if (titleA > titleB) {
+      return -1;
+    }
+    if (titleA < titleB) {
+      return 1;
+    }
+
+    // names must be equal
+    return 0;
+  }
+  ) : [];}
+
+if (atoZ) {
+  sortAtoZ();
+} else {
+  sortZtoA();
+}  
+
+function toggleSort() {
+  setAtoZ(!atoZ);
+}
 
   return (
     <div>
       <h1>Welcome to Test Store!</h1>
       {loading && <p>Loading...</p>}
       {error && <p style={{ color: 'red' }}>{error}</p>}
+      <input
+            type="text"
+            //value={'foo'}
+            // onChange={handleChange}
+            placeholder="search..."
+          />
+          <button type="submit">Search</button>
+          <button className='sortButton' onClick={toggleSort}><img src={atoZ?sortAZ:sortZA} alt="sort alphabetically"/></button>
+      <div className='main'>
       {data && !loading && !error && (
         data.map(product => (
           <Card key={product.id} image={product.image} title={product.title} price={product.price} desc={product.description}/>
         ))
-        // <Card image={data[0].image} title={data[0].title} price={data[0].price} desc={data[0].description}/>
+        
       )}
+      </div>
     </div>
   )
 }
