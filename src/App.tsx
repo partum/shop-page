3cb695a89ac0b599f5e1c03543a3714470009e02
Julia Arrington <jarring.dev@gmail.com>
2026-02-16 13:45:41 -0700

    added search feature

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import './App.css'
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useMemo } from 'react';
 import Card from './components/card'
 import sortAZ from './assets/alphabetical-sorting-icon-lg.png'
 import sortZA from './assets/alphabetical-sorting-icon-size_24.png'
@@ -10,6 +10,7 @@ interface Product {
   title: string;
   price: number;
   description: string;
+  category: string;
 }
 
 function App() {
@@ -17,6 +18,7 @@ function App() {
   const [error, setError] = useState<string | null>(null);
   const [data, setData] = useState<Product[] | null>(null);
   const [atoZ, setAtoZ] = useState(true);
+  const [query, setQuery] = useState("");
 
   const fetchData = () => {
     setLoading(true);
@@ -46,51 +48,69 @@ function App() {
     fetchData();
   }, []);
 
-//console.log('data', data);
+  //console.log('data', data);
 
-function sortAtoZ() {
-  const sortedData = data ? data.sort(
-    (a, b) => {
-    const titleA = a.title.toUpperCase(); // ignore upper and lowercase
-    const titleB = b.title.toUpperCase(); // ignore upper and lowercase
-    if (titleA < titleB) {
-      return -1;
-    }
-    if (titleA > titleB) {
-      return 1;
-    }
+  function sortAtoZ() {
+    const sortedData = data ? data.sort(
+      (a, b) => {
+        const titleA = a.title.toUpperCase(); // ignore upper and lowercase
+        const titleB = b.title.toUpperCase(); // ignore upper and lowercase
+        if (titleA < titleB) {
+          return -1;
+        }
+        if (titleA > titleB) {
+          return 1;
+        }
 
-    // names must be equal
-    return 0;
+        // names must be equal
+        return 0;
+      }
+    ) : [];
   }
-  ) : [];}
 
-function sortZtoA() {
-  const sortedData = data ? data.sort(
-    (a, b) => {
-    const titleA = a.title.toUpperCase(); // ignore upper and lowercase
-    const titleB = b.title.toUpperCase(); // ignore upper and lowercase
-    if (titleA > titleB) {
-      return -1;
-    }
-    if (titleA < titleB) {
-      return 1;
-    }
+  function sortZtoA() {
+    const sortedData = data ? data.sort(
+      (a, b) => {
+        const titleA = a.title.toUpperCase(); // ignore upper and lowercase
+        const titleB = b.title.toUpperCase(); // ignore upper and lowercase
+        if (titleA > titleB) {
+          return -1;
+        }
+        if (titleA < titleB) {
+          return 1;
+        }
 
-    // names must be equal
-    return 0;
+        // names must be equal
+        return 0;
+      }
+    ) : [];
   }
-  ) : [];}
 
-if (atoZ) {
-  sortAtoZ();
-} else {
-  sortZtoA();
-}  
+  if (atoZ) {
+    sortAtoZ();
+  } else {
+    sortZtoA();
+  }
 
-function toggleSort() {
-  setAtoZ(!atoZ);
-}
+  function toggleSort() {
+    setAtoZ(!atoZ);
+  };
+
+  // useEffect(() => {
+  //   if (atoZ) {
+  //     sortAtoZ();
+  //   } else {
+  //     sortZtoA();
+  //   }
+  // }
+  //   , [atoZ]);
+
+  //start of search
+  const filteredItems = useMemo(() => {
+    return data ? data.filter((item) =>
+      item.title.toLowerCase().includes(query.toLowerCase()) || item.category.toLowerCase().includes(query.toLowerCase()) || item.description.toLowerCase().includes(query.toLowerCase())
+    ) : [];
+  }, [query]);
 
   return (
     <div>
@@ -98,20 +118,21 @@ function toggleSort() {
       {loading && <p>Loading...</p>}
       {error && <p style={{ color: 'red' }}>{error}</p>}
       <input
-            type="text"
-            //value={'foo'}
-            // onChange={handleChange}
-            placeholder="search..."
-          />
-          <button type="submit">Search</button>
-          <button className='sortButton' onClick={toggleSort}><img src={atoZ?sortAZ:sortZA} alt="sort alphabetically"/></button>
+        id="search"
+        type="search"
+        placeholder="search..."
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+      />
+      {/* <button type="submit">Search</button> */}
+      <button className='sortButton' onClick={toggleSort}><img src={atoZ ? sortAZ : sortZA} alt="sort alphabetically" /></button>
       <div className='main'>
-      {data && !loading && !error && (
-        data.map(product => (
-          <Card key={product.id} image={product.image} title={product.title} price={product.price} desc={product.description}/>
-        ))
-        
-      )}
+        {data && !loading && !error && (
+          filteredItems.map(product => (
+            <Card key={product.id} image={product.image} title={product.title} category={product.category} price={product.price} desc={product.description} />
+          ))
+
+        )}
       </div>
     </div>
   )
--- src/components/card.tsx
+++ src/components/card.tsx
@@ -4,6 +4,7 @@ function Card(props) {
     return <span className='productCard'>
         <img src={props.image} alt="" />
         <h2>{props.title}</h2>
+        <p>{props.category}</p>
         <p>${props.price}</p>
         <p>{props.desc}</p>
     </span>;
