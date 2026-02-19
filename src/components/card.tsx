2d9000de7e1df2e02c3cca9112277087ded401ee
Julia Arrington <jarring.dev@gmail.com>
2026-02-19 16:47:42 -0700

    fixed 'any' type error

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -1,4 +1,12 @@
-function Card(props) {
+interface CardProps {
+    image: string;
+    title: string;
+    category: string;
+    price: number;
+    desc: string;
+}
+
+function Card(props: CardProps) {
     return <span className='productCard'>
         <img src={props.image} alt="" />
         <h2>{props.title}</h2>
